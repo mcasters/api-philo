78c6ef3016fef0b157fefeaea5d5a4b1c730ffba
Unknown <yonk@wanadoo.fr>
2020-09-30 07:28:28 +0200

    Add quote entity methods

--- src/controllers/quote.controller.js
+++ src/controllers/quote.controller.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
 
 import Quote from '../models/Quote';
-import Work from "../models/Work";
+import Work from '../models/Work';
+import Topic from '../models/Topic';
 
 export const create = async (req, res) => {
     if (!req.body) {
@@ -16,7 +17,7 @@ export const create = async (req, res) => {
             message: `Work not found.`
         });
     } else {
-        work.createQuote({text: req.body.text, part: req.body.part })
+        work.createQuote({text: req.body.text, part: req.body.part})
             .then(quote => res.send(quote))
             .catch(err => {
                 res.status(500).send({
@@ -54,6 +55,30 @@ export const findById = (req, res) => {
         });
 };
 
+export const findByTopic = async (req, res) => {
+    const topic = await Topic.findOne({where: {id: req.params.topicid}});
+    if (!topic) {
+        res.status(404).send({
+            message: `Topic not found.`
+        });
+    } else {
+        topic.getQuotes()
+            .then(quotes => res.send(quotes))
+            .catch(err => {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found quote.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: `Error retrieving quote.`
+                    });
+                }
+            });
+    }
+};
+
+
 export const findByWork = async (req, res) => {
     const work = await Work.findOne({where: {id: req.params.workid}});
     if (!work) {
@@ -61,7 +86,6 @@ export const findByWork = async (req, res) => {
             message: `Work not found.`
         });
     } else {
-        console.log(work);
         work.getQuotes()
             .then(quotes => res.send(quotes))
             .catch(err => {
@@ -101,6 +125,30 @@ export const update = (req, res) => {
                 });
             }
         })
+};
+
+export const removeTopic = async (req, res) => {
+    if (!req.body)
+        res.status(400).send({message: "Topicid can not be empty!"});
+
+    const quote = await Quote.findOne({where: {id: req.params.id}});
+    if (!quote)
+        res.status(400).send({message: "Quote not found"});
+
+    const topicToDelete = await quote.getTopics({where: {id: req.body.topicid}})
+    quote.removeTopic(topicToDelete)
+        .then(() => res.send({message: `Topic was removed successfully!`}))
+        .catch(err =>{
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Topic not found.`
+                });
+            } else {
+                res.status(500).send({
+                    message: err.message || `Could not remove topic`
+                });
+            }
+        });
 };
 
 export const deleteOne = (req, res) => {
--- src/routes/quote.routes.js
+++ src/routes/quote.routes.js
@@ -11,11 +11,17 @@ module.exports = app => {
     // Retrieve a single quote with id
     app.get("/quote/:id", quoteController.findById);
 
+    // Retrieve quotes by topic id
+    app.get("/quotes-by-topic/:topicid", quoteController.findByTopic);
+
     // Retrieve quotes by work id
-    app.get("/quotes/:workid", quoteController.findByWork);
+    app.get("/quotes-by-work/:workid", quoteController.findByWork);
 
     // Update a quote with id
     app.put("/quote/:id", quoteController.update);
+
+    // Remove a topic from a quote
+    app.put("/quote-remove-topic/:id", quoteController.removeTopic);
 
     // Delete a quote with id
     app.delete("/quote/:id", quoteController.deleteOne);
